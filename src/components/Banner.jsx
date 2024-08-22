3e0b9b8045605b2998697fec50efc3945f3ef63c
kais3r2023 <122757568+kais3r2023@users.noreply.github.com>
2024-08-21 23:14:16 -0500

    implementacion de componentes SocialBtn y  mailto

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,14 +6,14 @@ import githubIcon from "../images/githubIcon.svg";
 
 export default function Banner() {
   return (
-    <div className="h-screen w-1/2 p-24">
+    <div className="relative h-screen w-1/2 p-24">
       <h1 className="whitespace-nowrap text-5xl font-bold">
         Daniel Canales Taylor
       </h1>
       <h2 className="text-3xl font-bold text-sky-500">Web Developer</h2>
       <p className="whitespace-nowrap text-base text-gray-100">
-        “La perfección no es alcanzable, pero en su búsqueda podemos toparnos
-        con la excelencia”
+        “La perfección no es alcanzable, <br />
+        pero en su búsqueda podemos toparnos con la excelencia”
         <br /> Vince Lombardi.
       </p>
       <div className="mt-24 flex max-w-fit flex-col gap-y-2.5">
@@ -21,13 +21,18 @@ export default function Banner() {
         <NavigateBtn btnName={"TECNOLOGÍAS"} />
         <NavigateBtn btnName={"PROYECTOS"} />
       </div>
-      <div>
+      <div className="absolute bottom-0 mb-9 flex w-60 flex-row justify-between">
         <SocialBtn
           icon={linkedinIcon}
           link={"https://www.linkedin.com/in/daniel-canales-taylor-49466b285/"}
         />
-        <SocialBtn icon={emailIcon} />
         <SocialBtn icon={githubIcon} link={"https://github.com/kais3r2023"} />
+        <SocialBtn
+          icon={emailIcon}
+          link={
+            "mailto:danielcanalestaylor88@gmail.com?subject=Me%20interesa%20Contactarte%20como%20Desarrollador"
+          }
+        />
       </div>
     </div>
   );
--- src/components/SocialBtn.jsx
+++ src/components/SocialBtn.jsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+
 export default function SocialBtn({ link, icon }) {
+  const isMailto = link.startsWith("mailto:");
+
   return (
-    <a href={link} target="_blank" className="flex h-12 w-12">
+    <a
+      href={link}
+      target={isMailto ? "_self" : "_blank"}
+      className="flex h-12 w-12"
+    >
       <Image src={icon} alt="icono de red Social" className="h-12 w-12" />
     </a>
   );
